d1a86ecd75216878c77f96d9cb35b5ddc48b93a4
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-27 17:58:51 +0100

    fin du tuto :P

---
+++ scripts/conjuguetamere.php
@@ -0,0 +1,37 @@
+<?php
+
+$curl = curl_init('http://www.conjuguetamere.com/verbes-1er-groupe');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($curl);
+
+$buttonCount = count(explode('class="aPagination"', $response));
+$lastPage = $buttonCount ? $buttonCount - 2 : 1;
+
+$verbs = [];
+
+for ($page = 1; $page <= $lastPage; $page++) {
+
+    $curl = curl_init('http://www.conjuguetamere.com/verbes-1er-groupe?page=' . $page);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($curl);
+
+    $liSplit = explode('<li style="">', $response);
+
+    foreach ($liSplit as $liIndex => $liItem) {
+        if ($liIndex === 0) {
+            continue;
+        }
+
+        $link = explode('</li>', $liItem)[0];
+
+
+        $explodedLink = explode('conjuguetamere.com/verbe/', $link);
+        if (! isset($explodedLink[1])) {
+            continue;
+        }
+
+        $verbs[] = explode('"', $explodedLink[1])[0];
+    }
+}
+
+var_dump($verbs);
