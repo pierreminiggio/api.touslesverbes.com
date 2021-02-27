878a759885b2e2c343864ed64affddd83ab2428a
Pierre MINIGGIO <pierre.miniggio@gmail.com>
2021-02-27 23:37:31 +0100

    pull from all 3 groups

--- scripts/conjuguetamere.php
+++ scripts/conjuguetamere.php
@@ -1,37 +1,45 @@
 <?php
 
-$curl = curl_init('http://www.conjuguetamere.com/verbes-1er-groupe');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($curl);
+$groups = [
+    1 => 'http://www.conjuguetamere.com/verbes-1er-groupe',
+    2 => 'http://www.conjuguetamere.com/verbes-2eme-groupe',
+    3 => 'http://www.conjuguetamere.com/verbes-3eme-groupe'
+];
 
-$buttonCount = count(explode('class="aPagination"', $response));
-$lastPage = $buttonCount > 1 ? $buttonCount - 2 : 1;
-
-$verbs = [];
-
-for ($page = 1; $page <= $lastPage; $page++) {
-
-    $curl = curl_init('http://www.conjuguetamere.com/verbes-1er-groupe?page=' . $page);
+foreach ($groups as $groupId => $sourceUrl) {
+    $curl = curl_init($sourceUrl);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($curl);
 
-    $liSplit = explode('<li style="">', $response);
+    $buttonCount = count(explode('class="aPagination"', $response));
+    $lastPage = $buttonCount > 1 ? $buttonCount - 2 : 1;
 
-    foreach ($liSplit as $liIndex => $liItem) {
-        if ($liIndex === 0) {
-            continue;
+    $verbs = [];
+
+    for ($page = 1; $page <= $lastPage; $page++) {
+
+        $curl = curl_init($sourceUrl . '?page=' . $page);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+
+        $liSplit = explode('<li style="">', $response);
+
+        foreach ($liSplit as $liIndex => $liItem) {
+            if ($liIndex === 0) {
+                continue;
+            }
+
+            $link = explode('</li>', $liItem)[0];
+
+
+            $explodedLink = explode('conjuguetamere.com/verbe/', $link);
+            if (! isset($explodedLink[1])) {
+                continue;
+            }
+
+            $verbs[] = explode('"', $explodedLink[1])[0];
         }
-
-        $link = explode('</li>', $liItem)[0];
-
-
-        $explodedLink = explode('conjuguetamere.com/verbe/', $link);
-        if (! isset($explodedLink[1])) {
-            continue;
-        }
-
-        $verbs[] = explode('"', $explodedLink[1])[0];
     }
-}
 
-var_dump($verbs);
+    var_dump($verbs);
+}
